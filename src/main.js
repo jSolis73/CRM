382a92f8855478e917783c968777d509fd151866
unknown <chez90@mail.ru>
2019-06-06 20:49:03 +0400

    edit button dropdown, create filter to display time format

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import dateFilter from '@/filters/date.filter'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
-
+Vue.filter('date', dateFilter)
 new Vue({
   router,
   store,
