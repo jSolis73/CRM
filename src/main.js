78f11af42e9115ff52376f2a9ef6490626d9ee8c
unknown <chez90@mail.ru>
2019-06-09 13:30:20 +0400

    connect firebase, set interaction with server

--- src/main.js
+++ src/main.js
@@ -7,13 +7,33 @@ import './registerServiceWorker'
 import dateFilter from '@/filters/date.filter'
 import messagePlugin from '@/utils/message.plugin'
 import 'materialize-css/dist/js/materialize.min'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.use(Vuelidate)
 Vue.use(messagePlugin)
 Vue.config.productionTip = false
 Vue.filter('date', dateFilter)
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCTV3bIJ-7tIkWkJ81tLsAbkIjQEOm0XGI",
+  authDomain: "vue-crm73.firebaseapp.com",
+  databaseURL: "https://vue-crm73.firebaseio.com",
+  projectId: "vue-crm73",
+  storageBucket: "vue-crm73.appspot.com",
+  messagingSenderId: "598039368572",
+  appId: "1:598039368572:web:d985418a7ad20e3d"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -72,7 +72,7 @@ export default {
       this.$message(messages[this.$route.query.message])
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -81,7 +81,14 @@ export default {
         email: this.email,
         password: this.password
       }
-      this.$router.push('/')
+
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/')
+      } catch (e) {
+
+      }
+
     }
   }
 }
