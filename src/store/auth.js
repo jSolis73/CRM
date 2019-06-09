d9830c4a0187c28f1201bdd7d9c930c5148507b0
unknown <chez90@mail.ru>
2019-06-09 14:52:05 +0400

    add user registration

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app'
+
 export default {
   actions: {
     async login ({ dispatch, commit }, { email, password }) {
@@ -7,6 +8,25 @@ export default {
       } catch (e) {
         throw e
       }
+    },
+    async register ({dispatch}, {email, password, name}) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        const uid = await dispatch('getUid')
+        await firebase.database().ref(`/users/${uid}/info`).set({
+          bill: 1000,
+          name
+        })
+      } catch (e) {
+        throw e
+      }
+    },
+    getUid() {
+      const user = firebase.auth().currentUser
+      return user ? user.uid : null
+    },
+    async logout () {
+      await firebase.auth().signOut()
     }
   }
 }
